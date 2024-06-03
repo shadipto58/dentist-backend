cb6893b1eaaa0ab410c57a757f97a644032be7a2
shadipto mojumder <shadiptomojumder58k@gmail.com>
2024-06-03 11:06:58 +0600

    changes

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ import { app } from "./src/app.js";
 import connectDB from "./src/db/database.js";
 
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at PORT : ${process.env.PORT}`);
+try {
+  connectDB()
+    .then(() => {
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+      });
+      app.on("error", (error) => {
+        console.log("Error in App", error);
+        throw error;
+      });
+    })
+    .catch((error) => {
+      console.log("MONGODB CONNECTION ERROR !!", error);
     });
-    app.on("error", (error) => {
-      console.log("Error in App", error);
-      throw error;
-    });
-  })
-  .catch((error) => {
-    console.log("MONGODB CONNECTION ERROR !!", error);
-  });
+} catch (error) {
+  console.log("Error in index.js", error);
+}
